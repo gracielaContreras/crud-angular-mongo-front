52cd6d985f6f04a930a79d0f61381dd6f6255f83
Graciela Contreras <gracielaestefani@gmail.com>
2023-05-12 02:10:52 -0400

    Creando las rutas

--- crud-angular-mongo-front/src/app/app-routing.module.ts
+++ crud-angular-mongo-front/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListComponent } from './product/list.component';
+import { DetailComponent } from './product/detail.component';
+import { CreateComponent } from './product/create.component';
+import { UpdateComponent } from './product/update.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: ListComponent},
+  {path: 'detail', component: DetailComponent},
+  {path: 'create', component: CreateComponent},
+  {path: 'update', component: UpdateComponent},
+  // ruta para los errores (Los redigerimos a la raiz)
+  {path: '**', redirectTo: '', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
