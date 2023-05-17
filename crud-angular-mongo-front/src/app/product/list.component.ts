48d65a2a148fb6e21ed65e14c1ee5be2be92bd4d
Graciela Contreras <gracielaestefani@gmail.com>
2023-05-17 18:05:12 -0400

    Se agrega configuración para obtener los datos del backend.

--- crud-angular-mongo-front/src/app/product/list.component.ts
+++ crud-angular-mongo-front/src/app/product/list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../services/product.service';
+import { ToastrService } from 'ngx-toastr';
+import { Product } from '../model/product';
 
 @Component({
   selector: 'app-list',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
-  constructor() { }
+  // Inicializando una lista vacia de productos
+  products: Product[] = [];
+
+  constructor(
+    private productService: ProductService,
+    private toast:ToastrService
+  ) { }
 
   ngOnInit(): void {
+      //llamar metodos
+      this.getProducts();
+
   }
+
+  getProducts(): void{
+    this.productService.list().subscribe(
+      data => {
+        this.products = data;
+        console.log(this.products);
+      },
+      err => {
+        this.toast.error(err.error.message, 'Error', { timeOut: 3000, positionClass: 'toast-top-center'});
+      }
+    );
+  }
+
 
 }
--- crud-angular-mongo-front/src/app/services/product.service.ts
+++ crud-angular-mongo-front/src/app/services/product.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Product } from '../model/product';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  productURL= environment.apiResrURL + '/api/v1/product'
+
+  constructor(private httpClient: HttpClient) { }
+
+  //Obtenemos la lista de productos desde el back
+  public list(): Observable<Product[]>{
+    return this.httpClient.get<Product[]>(this.productURL)
+  }
 }
